585f077b43f4c7c62e5694cf033d931bdf51c2cb
anoj-pro <160610761+anoj-pro@users.noreply.github.com>
2024-02-20 19:25:48 +0545

    portfolio
    To Uday Raj Karki

---
+++ anoj.js
@@ -0,0 +1,8 @@
+/*===============================typing  animation================*/
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer", "web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
